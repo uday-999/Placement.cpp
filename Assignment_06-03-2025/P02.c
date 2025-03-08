d2f4bac76d7c41ebb9eda68816b63a2d92861e5a
uday-999 <skiddiee99@gmail.com>
2025-03-08 19:11:26 +0530

    P02.c

--- Assignment_06-03-2025/P02.c
+++ Assignment_06-03-2025/P02.c
@@ -1,1 +1,18 @@
+//WAP to input two numbers and print all prime numbers in range
 
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	int a,b,i,j;
+	printf("Enter the range ");
+	scanf("%d %d",&a,&b);
+	for(i=a;i<=b;i++){
+		for(j=2;j<=i;j++)
+		{
+			if(i%j==0)
+			break;
+		}
+		if(i==j)
+		printf("%d \n",j);
+	}
+}
